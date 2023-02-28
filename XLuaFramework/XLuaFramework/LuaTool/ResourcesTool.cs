d5a8d25c25ceb0120d99e28d717f6f06cc133215
habarajiang <Irisu_M@outlook.com>
2023-02-28 21:06:10 +0800

    优化资源管理池，避免显示gameobject资源

--- XLuaFramework/XLuaFramework/LuaTool/ResourcesTool.cs
+++ XLuaFramework/XLuaFramework/LuaTool/ResourcesTool.cs
@@ -11,6 +11,12 @@ namespace XLuaFramework.LuaTool
 {
     internal static class ResourcesTool
     {
+        static GameObject res_root = new GameObject("res root");
+        static ResourcesTool()
+        {
+            Object.DontDestroyOnLoad(res_root);
+            res_root.SetActive(false);
+        }
         static Dictionary<string, Object> ResPool = new Dictionary<string, Object>();
         public static Object GetPoolObject(string path)
         {
@@ -21,6 +27,10 @@ namespace XLuaFramework.LuaTool
         public static void AddPoolObject(string path,Object res)
         {
             Object.DontDestroyOnLoad(res);
+            if((res as GameObject) != null)
+            {
+                (res as GameObject).transform.parent = res_root.transform;
+            }
             ResPool[path] = res;
         }
     }
