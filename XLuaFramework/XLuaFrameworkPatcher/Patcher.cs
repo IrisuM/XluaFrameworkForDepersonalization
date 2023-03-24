8ee1b7ddfce65e7c54e666036d6ba21ef88a36c7
habarajiang <Irisu_M@outlook.com>
2023-03-24 11:35:24 +0800

    添加类监控功能

--- XLuaFramework/XLuaFrameworkPatcher/Patcher.cs
+++ XLuaFramework/XLuaFrameworkPatcher/Patcher.cs
@@ -7,6 +7,8 @@ using System.Text;
 using Mono.Cecil.Rocks;
 using System.Reflection;
 using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace XLuaFrameworkPatcher
 {
@@ -14,12 +16,25 @@ namespace XLuaFrameworkPatcher
     {        // List of assemblies to patch
         public static IEnumerable<string> TargetDLLs => GetDLLs();
 
+        static void TestPrint(string msg)
+        {
+
+        }
         // Patches the assemblies
         //定制化修改，随便写写
         //把加载资源全部改为缓存
         public static void Patch(ref AssemblyDefinition assembly)
         {
-            
+            switch (assembly.Name.Name)
+            {
+                case "Assembly-CSharp":
+                    {
+                        new AccessMonitor(assembly, "MOD.CareerData", TestPrint).Monitor();
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
 
         public static IEnumerable<string> GetDLLs()
